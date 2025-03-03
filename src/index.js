e0f1f2e445e7d88bd8a564786e4a419db92a02d7
Jatin shukla <jatinshuklaop1234@gmail.com>
2025-03-03 22:40:03 +0530

    add changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ import connectDB from "./DB/index.js";
  dotenv.config({
     path:'./env'
  })
-connectDB();
+
+
+
+connectDB()
+.then(()=>{
+   app.listen(process.env.PORT|| 8000,()=>{
+      console.log(`Server is runing at :${process.env.PORT}`);
+      
+   })
+})
+.catch((err)=>{
+   console.log("MONOGO db connnection falied!",err);
+   
+   
+})
+
 
 // import express from "express";
 // const app =express();
