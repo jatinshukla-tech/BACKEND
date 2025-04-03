f2d8cdaef8183f58f3411f5356fd3bdf86a06466
Jatin shukla <jatinshuklaop1234@gmail.com>
2025-04-03 19:55:05 +0530

    bugs fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
+import { upload } from "../middlewares/multer.js";
 import {resgisterUser} from "../controllers/user.controller.js";
  
 const router =Router() 
-router.route('/register').post(resgisterUser)
+
+
+
+router.route('/register').post
+      upload.fields
+        ({name:"avatar",
+            maxCount: 1
+
+        }
+       ,{
+        name:"coverImages",
+        maxCount :1
+       }
+
+)(resgisterUser)
 
 
 
